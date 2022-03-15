536a5e3bf40f59bf240e4a9b10ad82e1433c11c9
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-03-15 21:45:30 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (54.52%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        int arr[] = new int[n+1];
+        for(int i = 0 ; i < n+1 ; i++ ){
+            arr[i] = -1;
+        }
+        return climb(n,0,arr);
+    }
+    
+    public int climb(int n,int cur,int[] arr){
+        if(cur < arr.length && arr[cur] != -1) return arr[cur];
+        
+        if(cur > n) return 0;
+        if(cur == n) return 1;
+        
+        int ans =  climb(n,cur+1,arr)+climb(n,cur+2,arr);
+        
+        arr[cur]  = ans;
+        
+        return ans;
+    }
+}
