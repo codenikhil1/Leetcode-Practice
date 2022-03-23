5fe6e98cfecb79c113b53509e3058bd00888f033
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-03-23 22:41:24 +0530

    Time: 1313 ms (5.77%), Space: 374.2 MB (5.05%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int len = prices.length;
-        int max_prof = 0;
-        int low = Integer.MAX_VALUE;
+       // Integer[][][] dp = new Integer[current+1][2][2]; 
+        return profit(prices,0,0,1,new HashMap<String,Integer>());
+    }
+    
+    private int profit(int[] prices,int current,int canIBuy,int                                                             noOfTransaction,HashMap<String,Integer> map){
+        if(current >= prices.length || noOfTransaction == 0 ) return 0;
         
-        for(int i = 0 ; i < len-1 ; i++ ){
-            low = Math.min(low,prices[i]);
-            int profit = prices[i+1] - low;
+        int buyStock = 0;
+        int sellStock = 0;
+        
+        String key = current + "-" + canIBuy + "-" + noOfTransaction;
+        
+        if(map.containsKey(key)) return map.get(key);
+        
+        int Idle = profit(prices,current+1,canIBuy,noOfTransaction,map);
+        
+        if(canIBuy == 0)
+            buyStock = -prices[current]+profit(prices,current+1,1,1,map);
+        else
+            sellStock = prices[current]+profit(prices,current+1,0,0,map);
+        
+        map.put(key,Math.max(Idle,Math.max(buyStock,sellStock)));
+        return map.get(key);
+        
             
-            max_prof = Math.max(profit,max_prof);
-        }
         
-        return max_prof;
+        
     }
 }
