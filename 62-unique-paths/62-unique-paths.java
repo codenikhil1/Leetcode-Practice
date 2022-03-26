c8e86108b66b337cadb4637623928a29a1f4cd97
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-03-26 21:28:39 +0530

    Time: 1 ms (46.06%), Space: 41.9 MB (5.98%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        Integer dp[][] = new Integer[m+1][n+1];
+        return ways(m,n,0,0,dp);
+    }
+    
+    private int ways(int m,int n,int x, int y,Integer dp[][]){
+        if(x >= m || y >= n) return 0;
+        
+        if(x == m-1 && y == n-1 ) return 1;
+        
+        if(dp[x][y] != null) return dp[x][y];
+        
+        int right = ways(m,n,x,y+1,dp);
+        int down = ways(m,n,x+1,y,dp);
+        
+        dp[x][y] = right+down;
+        
+        return right+down;
+        
+        
+    }
+}
