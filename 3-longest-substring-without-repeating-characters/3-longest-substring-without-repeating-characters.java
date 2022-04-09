3d740c5dff3650c1c954439a1d2a64a17740bdfd
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-04-09 13:34:30 +0530

    Time: 4 ms (95.79%), Space: 42.3 MB (92.69%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -7,13 +7,12 @@ class Solution {
         for(int i = 0 ; i < s.length() ; i++ ){
             char currentChar = s.charAt(i);
             
-            while(map.containsKey(currentChar)){
-                char discartChar = s.charAt(release);
-                map.remove(discartChar);
-                release++;
+            if(map.containsKey(currentChar) && map.get(currentChar) >= release){
+                int index = map.get(currentChar);
+                release = index+1;
             }
             map.put(currentChar,i);
-            int tempAns = (i - release) + 1;
+            int tempAns = (i - release) + 1;    
             ans = Math.max(ans,tempAns);
         }
         
