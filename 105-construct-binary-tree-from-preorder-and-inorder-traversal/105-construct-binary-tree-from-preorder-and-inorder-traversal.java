44f3f3d23091eddbf5fce791c27e649b70e27e23
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-05-12 21:29:31 +0530

    Time: 3 ms (81.01%), Space: 44.8 MB (23.25%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,23 +16,26 @@
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int preIndex[] = {0};
-        return constructTree(preorder,inorder,preIndex,0,inorder.length - 1);
+        HashMap<Integer,Integer> inOrderIndexMap = new HashMap<>();
+        buildMap(inOrderIndexMap,inorder);
+        return constructTree(preorder,inOrderIndexMap,preIndex,0,inorder.length - 1);
     }
-    private TreeNode constructTree(int[] preorder,int[] inorder,int[] preIndex,int inStart,int inEnd){
+    private void buildMap(HashMap<Integer,Integer> inOrderIndexMap,int[] inorder){
+        
+        for(int i = 0 ; i < inorder.length ; i++ )
+            inOrderIndexMap.put(inorder[i],i);
+    }
+    private TreeNode constructTree(int[] preorder,HashMap<Integer,Integer> inOrderIndexMap,int[] preIndex,int inStart,int inEnd){
         if(preIndex[0] > preorder.length || inStart > inEnd)
             return null;
         
         int currentVal = preorder[preIndex[0]];
         TreeNode node = new TreeNode(currentVal);
         preIndex[0]++;
-        int i = -1;
-        for(i = inStart ; i <= inEnd ; i++){
-            if(inorder[i] == currentVal)
-                break;
-        }
+        int i = inOrderIndexMap.get(currentVal);
         
-        node.left = constructTree(preorder,inorder,preIndex,inStart,i-1);
-        node.right = constructTree(preorder,inorder,preIndex,i+1,inEnd);
+        node.left = constructTree(preorder,inOrderIndexMap,preIndex,inStart,i-1);
+        node.right = constructTree(preorder,inOrderIndexMap,preIndex,i+1,inEnd);
         
         return node;
     }
