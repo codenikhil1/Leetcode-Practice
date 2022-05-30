dd747ad6f7988b2b75c53260a5ee7a8d105139fa
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-05-30 22:14:07 +0530

    Time: 10 ms (72.00%), Space: 41.8 MB (40.24%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -74,9 +74,9 @@ class Solution {
         int x = 3 * (currentRow/3);
         int y = 3 * (currentCol/3);
         
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(board[x+i][y+j] == currentVal){
+        for(int i = x; i < x+3; i++){
+            for(int j = y; j < y+3; j++){
+                if(board[i][j] == currentVal){
                     return false;
                 }
             }
