7269c28c8b14a93b868326af6670a6dcecc033ae
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-06-11 13:51:18 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] visited = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
+        
+        q.add(0);
+        int vertexCount = V;
+        while(q.size() != 0){
+            int currentVertex = q.remove();
+            if(visited[currentVertex])
+                continue;
+                
+            ans.add(currentVertex);
+            visited[currentVertex] = true;
+            
+            ArrayList<Integer> neighbours = adj.get(currentVertex);
+            
+            for(int neighbour : neighbours){
+                if(visited[neighbour] == false)
+                    q.add(neighbour);
+            }
+            
+        }
+        
+        return ans;
+    }
+}
