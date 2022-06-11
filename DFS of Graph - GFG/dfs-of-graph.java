9ece7c46e562ffb945e3b8d1118be6287201f202
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-06-11 11:52:06 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,66 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        int[] visited = new int[V];
+        ArrayList<Integer> ans = new ArrayList<>();
+        dfs(V,adj,ans,0,visited);
+        
+        return ans;
+    }
+    
+    public void dfs(int V, ArrayList<ArrayList<Integer>> adj,
+        ArrayList<Integer> ans,int currentVertex,int[] visited){
+        
+    if(visited[currentVertex] == 1)
+        return;
+        
+    ArrayList<Integer> vertexList = adj.get(currentVertex);
+    visited[currentVertex] = 1;
+    ans.add(currentVertex);
+    for(int vertex : vertexList){
+        dfs(V,adj,ans,vertex,visited);
+    }
+    
+    return;
+        
+        
+        
+    }
+    
+}
