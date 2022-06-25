558f3c936f949f96da706363ab469942348e3a69
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-06-25 12:08:08 +0530

    Time: 5 ms (91.36%), Space: 43.5 MB (86.15%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = constructGraph(numCourses,prerequisites);
+        
+        int[] inDegree = new int[numCourses];
+        boolean[] visited = new boolean[numCourses];
+        
+        for(int i = 0 ; i < numCourses ; i++ ){
+            for(int vertex : adj.get(i))
+                inDegree[vertex]++;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        for(int i = 0 ; i < numCourses;i++){
+            if(inDegree[i] == 0){
+                queue.add(i);
+            }
+                
+        }
+        
+        
+        
+        while(!queue.isEmpty()){
+            int course = queue.remove();    
+                
+            if(visited[course])
+                continue;
+            
+            visited[course] = true;
+            ans.add(course);
+            
+            for(int neigh : adj.get(course)){
+                inDegree[neigh]--;
+                if(inDegree[neigh] == 0)
+                    queue.add(neigh);
+            }
+            
+        }
+        
+        if(ans.size() != numCourses)
+            return new int[0];
+        
+        int[] ansArr = new int[ans.size()];
+        
+        for(int i = 0 ; i < ansArr.length ; i++)
+            ansArr[i] = ans.get(i);
+        
+        return ansArr;
+        
+    }
+    
+    public ArrayList<ArrayList<Integer>> constructGraph(int n , int[][] prerequisites){
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        
+        for(int i = 0 ; i < n ; i++ )
+            adj.add(new ArrayList<Integer>());
+        
+        for(int edge[] : prerequisites ){
+            int a = edge[0];
+            int b = edge[1];
+            // must take course b first if you want to take course a.
+            adj.get(b).add(a);
+        }
+        
+        return adj;
+    }
+}
