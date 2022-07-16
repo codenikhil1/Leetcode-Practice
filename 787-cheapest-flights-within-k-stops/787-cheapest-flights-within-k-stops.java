829a6ea7cfe108f5ef8bf8fbaea76fdc79068918
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-07-16 13:20:49 +0530

    Time: 8 ms (61.98%), Space: 47.2 MB (48.88%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -11,6 +11,12 @@ class Solution {
       for(int i = 0 ; i <= k ; i++ ){
           
         //use temp array
+        //update temp array and dont update cost array
+        //we are relaxing each edge in iteration
+        // if we dont have temp array , we might consider more than k stops to reach dest
+        // will always update temp array , to avoid more than k stops
+        // cost holds price to reach dest k-1 times and for kth time will use temp and copy temp in cost 
+         // watch following video and think, you will get to know
         //why we use temp array - https://www.youtube.com/watch?v=5eIK3zUdYmE
         int[] temp = Arrays.copyOf(cost,n);
           
