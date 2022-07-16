847980d4d2b6f0fc910fddceaf474e7bc908e803
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2022-07-16 17:53:17 +0530

    Time: 1 ms (88.90%), Space: 54 MB (29.52%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,19 +1,26 @@
 class Solution {
     
+    /*
 
+    */
     
     public boolean isBipartite(int[][] graph) {
         
         int n = graph.length;
+        //maintain color of each vertex either 0 or 1;
         int colors[] = new int[n];
         Arrays.fill(colors,-1);
         
+        
+        //go through each vertex
         for(int currentVertex = 0 ; currentVertex < n ; currentVertex++ ){
             
+            //if vertex is already colored then continue
             if(colors[currentVertex] != -1)
                 continue;
             
-            //dfs
+            //our logic here
+            //dfs on each vertex 
             if(!hasEvenCycle(graph,currentVertex,colors,0))
                 return false;
             
@@ -26,15 +33,21 @@ class Solution {
     
     public boolean hasEvenCycle(int[][] graph,int currentVertex,int[] colors,int currentColor){
         
+        //if vertex is already colored
+        //if color is not a color that we were expecting means if other vertex which is connected to this vertex
+        //has 0 then this should have 1
+        // if thats not case then return false
         if(colors[currentVertex] != -1)
             return colors[currentVertex] == currentColor;
         
+        //color the vetex
         colors[currentVertex] = currentColor;
         
         for(int neigh : graph[currentVertex]){
             
             //dfs
             // 1-currentColor will toggle color 0-1
+            //call recursiverly with neighbour toggling the color
             if(!hasEvenCycle(graph,neigh,colors,1-currentColor))
                 return false;
         }
